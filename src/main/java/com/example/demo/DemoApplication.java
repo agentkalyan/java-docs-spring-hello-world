032b016d5e5a0612e7dc373920bab824e178253a
agentkalyan <122280633+agentkalyan@users.noreply.github.com>
2023-01-09 18:38:57 +0530

    my commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,8 +13,8 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@RequestMapping("/")
+	@RequestMapping("/optum")
 	String sayHello() {
-		return "Hello World!";
+		return "Punakalu Loading!";
 	}
 }
